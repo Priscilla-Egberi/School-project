a10ea6cf472efc75e1afe802cf98dd4224a2aff1
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-21 20:13:22 +0100

    editing the addItem page

--- src/components/account/AddNewItem.js
+++ src/components/account/AddNewItem.js
@@ -7,7 +7,7 @@ function AddNewItem() {
       <h1 className="mt-7 text-xl font-bold tracking-tight text-gray-900 text-center">What do you want to add?</h1>
       <div className="flex flex-row justify-center">
         {/* card 1 */}
-        <div className="max-w-sm p-5 flex flex-col justify-center items-center rounded shadow-lg mr-5">
+        <div className="max-w-xs p-5 flex flex-col justify-center items-center rounded shadow-lg mr-5">
             <div className='rounded-full bg-my-lightorange p-3'>
             <img src='https://img.freepik.com/free-vector/vintage-tv_23-2147503075.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-5 h-5' />
             </div>
@@ -15,7 +15,7 @@ function AddNewItem() {
             <p className="text-center">Sell both new and fairly used products of any kind</p>
         </div>
         {/* card 2 */}
-        <div className="max-w-sm p-5 flex flex-col justify-center items-center rounded shadow-lg ml-5">
+        <div className="max-w-xs p-5 flex flex-col justify-center items-center rounded shadow-lg ml-5">
             <div className='rounded-full bg-my-lightorange p-3'>
             <img src='https://img.freepik.com/free-vector/cheerful-cute-dog-white-background_1308-132745.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-5 h-5' />
             </div>
