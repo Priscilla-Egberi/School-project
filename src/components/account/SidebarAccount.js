d17be5b4a178b2b72e9fc92717e8f58ab5b88567
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-21 08:08:50 +0100

    adding the routing feature to my account page

--- src/components/account/SidebarAccount.js
+++ src/components/account/SidebarAccount.js
@@ -14,10 +14,10 @@ function SideBar() {
     <section className='h-full bg-white flex flex-col justify-between border-r-gray border-2'>
         <div>
               <div id="title" className="flex flex-row items-center text-white p-2  h-16" style={{backgroundColor:"#EB6B39"}}> <AiOutlineUserAdd className="mr-2"/> Account</div>
-              <Link to="/additems"  className=" flex flex-row items-center justify-center py-3 text-my-orange bg-my-lightorange"> <AiOutlinePlus className="mr-2" /> Add New Item</Link>
+              <Link to="/account/additems"  className=" flex flex-row items-center justify-center py-3 text-my-orange bg-my-lightorange"> <AiOutlinePlus className="mr-2" /> Add New Item</Link>
               <div className="px-4  flex flex-col gap-y-3">        
-                  <Link to="/" className="flex items-center"><AiOutlineUser className="mr-2"/> My Profile</Link>
-                  <Link to="/clothings" className="flex items-center"><BsBagDash  className="mr-2"/> My Goods</Link>
+                  <Link to="/account" className="flex items-center"><AiOutlineUser className="mr-2"/> My Profile</Link>
+                  <Link to="/account/goods" className="flex items-center"><BsBagDash  className="mr-2"/> My Goods</Link>
                   <p className="flex items-center"><RiHandCoinLine className="mr-2"/> My Services</p>
                   <p className="flex items-center"><IoStatsChartOutline className="mr-2"/> Insights</p>
                   <p className="flex items-center"><GoVerified className="mr-2"/> Verification</p>
