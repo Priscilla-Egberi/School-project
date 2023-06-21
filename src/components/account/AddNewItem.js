5c51ab253fb70cb4e8d3d1ccf124f699c018920f
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-21 20:15:28 +0100

    editing the addItem page

--- src/components/account/AddNewItem.js
+++ src/components/account/AddNewItem.js
@@ -4,14 +4,14 @@ function AddNewItem() {
   return (
     <div>
       <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Add New Item</p>
-      <h1 className="mt-7 text-xl font-bold tracking-tight text-gray-900 text-center">What do you want to add?</h1>
+      <h1 className="my-7 text-xl font-bold tracking-tight text-gray-900 text-center">What do you want to add?</h1>
       <div className="flex flex-row justify-center">
         {/* card 1 */}
         <div className="max-w-xs p-5 flex flex-col justify-center items-center rounded shadow-lg mr-5">
             <div className='rounded-full bg-my-lightorange p-3'>
             <img src='https://img.freepik.com/free-vector/vintage-tv_23-2147503075.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-5 h-5' />
             </div>
-            <h1 className="text-xl font-bold tracking-tight text-gray-900 text-center">Sell Product</h1>
+            <h1 className="my-3 text-xl font-bold tracking-tight text-gray-900 text-center">Sell Product</h1>
             <p className="text-center">Sell both new and fairly used products of any kind</p>
         </div>
         {/* card 2 */}
@@ -19,7 +19,7 @@ function AddNewItem() {
             <div className='rounded-full bg-my-lightorange p-3'>
             <img src='https://img.freepik.com/free-vector/cheerful-cute-dog-white-background_1308-132745.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-5 h-5' />
             </div>
-            <h1 className="text-xl font-bold tracking-tight text-gray-900 text-center">Offer Service</h1>
+            <h1 className="my-3 text-xl font-bold tracking-tight text-gray-900 text-center">Offer Service</h1>
             <p className="text-center">Render any kind of service you can offer to anyone</p>
         </div>
       </div>
