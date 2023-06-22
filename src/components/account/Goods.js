884f4cb57653b88fe8d19218bb8c63c2da0dadbf
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-22 15:53:28 +0100

    edited the goods components in the account page

--- src/components/account/Goods.js
+++ src/components/account/Goods.js
@@ -11,12 +11,22 @@ function Goods() {
             </div>
             <div className="grid grid-cols-2 gap-x-6">
             {/* card 1 */}
-            <div className="max-w-xs p-5 flex flex-row justify-center items-center rounded shadow-lg">
-                <div className='rounded-full bg-my-lightorange p-3'>
-                <img src='https://img.freepik.com/free-vector/vintage-tv_23-2147503075.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-5 h-5' />
+            <div className="max-w-xs py-4 flex flex-row justify-center border-b-2 border-gray">
+                <div className='flex flex-row'>
+                      <img src='https://images.unsplash.com/photo-1601524909162-ae8725290836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcCUyMGNvbXB1dGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+                      alt='' className='h-full' />
+                      <div className="flex flex-col justify-evenly">
+                        <p>Shirt</p>
+                        <p>Red color</p>
+                        <p>Large</p>
+                      </div>
                 </div>
-                <h1 className="my-3 text-xl font-bold tracking-tight text-gray-900 text-center">Sell Product</h1>
-                <p className="text-center">Sell both new and fairly used products of any kind</p>
+
+                <div className="flex flex-col justify-between">
+                  <p>...</p>
+                  <p className="">Not Available</p>
+                </div>
+                
             </div>
             {/* card 2 */}
             <div className="max-w-xs p-5 flex flex-row justify-center items-center rounded shadow-lg">
