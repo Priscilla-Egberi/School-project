e43b7266a317ee103c8da58bb2b0ce82a9ce04f8
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-22 16:38:37 +0100

    edited the goods components in the account page

--- src/components/account/Goods.js
+++ src/components/account/Goods.js
@@ -11,22 +11,24 @@ function Goods() {
             </div>
             <div className="grid grid-cols-2 gap-x-6">
             {/* card 1 */}
-            <div className="max-w-xs py-4 flex flex-row justify-center border-b-2 border-gray">
+            <div className="max-h-40 py-4 flex flex-row justify-center border-b-2 border-gray text-gray-500 font-semibold">
                 <div className='flex flex-row'>
                       <img src='https://images.unsplash.com/photo-1601524909162-ae8725290836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcCUyMGNvbXB1dGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-                      alt='' className='h-full' />
-                      <div className="flex flex-col justify-evenly">
-                        <p>Shirt</p>
-                        <p>Red color</p>
-                        <p>Large</p>
+                      alt='' className='h-full rounded mr-4' />
+                      <div className="flex flex-col justify-between">
+                        <div className="text-sm">
+                            <p className="text-gray-700 text-base">Shirt</p>
+                            <p>Red color</p>
+                            <p>Large</p>
+                        </div>
+                            <p>$100</p>
                       </div>
                 </div>
 
-                <div className="flex flex-col justify-between">
+                <div className="flex flex-col justify-between text-right">
                   <p>...</p>
-                  <p className="">Not Available</p>
+                  <p className="text-blue-700">Not Available</p>
                 </div>
-                
             </div>
             {/* card 2 */}
             <div className="max-w-xs p-5 flex flex-row justify-center items-center rounded shadow-lg">
