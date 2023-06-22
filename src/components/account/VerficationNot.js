8b61e84d1b39ba4bef419cc43fe88ebee1a73382
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-22 14:27:57 +0100

    working on the verification component of the account page

--- src/components/account/VerficationNot.js
+++ src/components/account/VerficationNot.js
@@ -6,13 +6,13 @@ function VerficationNot() {
     <div>
       <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Verification</p>
       
-        <div className="mt-14 max-w-sm p-5 flex flex-col justify-center items-center ">
+        <div className="mx-auto mt-14 max-w-sm p-5 flex flex-col justify-center items-center ">
             <div className='rounded-full bg-my-lightorange p-6'>
             <img src='https://img.freepik.com/free-vector/vintage-tv_23-2147503075.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-10 h-10' />
             </div>
             <h1 className="my-3 text-xl font-bold tracking-tight text-gray-900 text-center">Your account is currently not verified</h1>
             <p className="text-center">You cannot sell on this platform without verifying your account as a student.<br/>Please providethe necessary requirements to indicate that you are a student of university of Uyo</p>
-            <Button className="mt-4 mx-3 rounded-sm flex flex-row items-center justify-center py-3 text-my-orange bg-my-orange"> Verify your account</Button>
+            <Button className="mt-4 mx-3 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"> Verify your account</Button>
         </div>
        
       </div>
--- src/components/account/Verified.js
+++ src/components/account/Verified.js
@@ -5,7 +5,7 @@ function Verified() {
     <div>
       <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Verification</p>
       
-        <div className="mt-14 max-w-sm p-5 flex flex-col justify-center items-center ">
+        <div className="mx-auto mt-14 max-w-sm p-5 flex flex-col justify-center items-center ">
             <div className='rounded-full bg-my-lightorange p-6'>
             <img src='https://img.freepik.com/free-vector/vintage-tv_23-2147503075.jpg?size=626&ext=jpg&ga=GA1.2.1841543944.1687297578&semt=sph' alt='' className='w-10 h-10' />
             </div>
