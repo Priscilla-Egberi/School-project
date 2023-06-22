c46de59db72b57cb1aa05e10bc96e60651a80c82
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-22 16:42:21 +0100

    edited the goods components in the account page

--- src/components/account/Goods.js
+++ src/components/account/Goods.js
@@ -11,7 +11,7 @@ function Goods() {
             </div>
             <div className="grid grid-cols-2 gap-x-6">
             {/* card 1 */}
-            <div className="max-h-40 py-4 flex flex-row justify-center border-b-2 border-gray text-gray-500 font-semibold">
+            <div className="max-h-40 py-4 flex flex-row justify-between border-b-2 border-gray text-gray-500 font-semibold">
                 <div className='flex flex-row'>
                       <img src='https://images.unsplash.com/photo-1601524909162-ae8725290836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcCUyMGNvbXB1dGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
                       alt='' className='h-full rounded mr-4' />
