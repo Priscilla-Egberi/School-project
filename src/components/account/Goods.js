282f0000881d3ba837ce2a6386492cdf5aceec7d
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-06-22 16:54:49 +0100

    edited the goods components in the account page

--- src/components/account/Goods.js
+++ src/components/account/Goods.js
@@ -6,7 +6,7 @@ function Goods() {
     <div>
     <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Goods</p>
     <div className="mx-10">
-            <div className="flex flex-row gap-x-4 font-bold mt-10">
+            <div className="flex flex-row gap-x-4 font-bold mt-10 border-b-2 border-gray-700">
                 <p className='border-b-2 border-my-orange'>Available</p>
                 <p>Not-available</p>
             </div>
