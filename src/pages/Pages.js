dc6db901ebc3c80b4a37595a5d2cded82dee6bcc
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-01 12:28:04 +0100

    editing service components in the account page

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -14,6 +14,7 @@ import AddItem from "../components/account/AddNewItem"
 import VerficationNot from '../components/account/VerficationNot'
 import Verified from '../components/account/Verified'
 import Goods from '../components/account/Goods'
+import Services from '../components/account/Servicess'
 
 
 export default function pages() {
@@ -35,6 +36,7 @@ export default function pages() {
                 <Route path="/account/verification" element={<VerficationNot />}/>
                 <Route path="/account/verified" element={<Verified />}/>
                 <Route path="/account/goods" element={<Goods />}/>
+                <Route path="/account/services" element={<Services />}/>
         </Route>
       </Routes>
       
