04cb67d747801f5c5ceb879eefdf86c59afbbc1d
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-01 12:29:06 +0100

    editing service components in the account page

--- src/components/account/Services.js
+++ src/components/account/Services.js
@@ -4,7 +4,7 @@ import Card from "./GoodsCard"
 function Goods() {
   return (
     <div>
-    <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Goods</p>
+    <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Services</p>
     <div className="mx-10">
             <div className="flex flex-row gap-x-4 font-bold mt-10 border-b-2 border-gray-700">
                 <p className='border-b-2 border-my-orange'>Available</p>
