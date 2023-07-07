74b3ace992501fc91fe52cfe3d664be1e7f1d7c7
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-07 10:20:06 +0100

    working on AddGoods Component of the Account

--- src/components/account/AddGoods.js
+++ src/components/account/AddGoods.js
@@ -41,16 +41,16 @@ function AddGoods() {
                                         d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                 </svg>
                                 <span className="font-medium text-gray-600">
-                                    Drop files to Attach, or
+                                    Drop Item's Image to Attach, or
                                     <span className="text-blue-600 underline">browse</span>
                                 </span>
                             </span>
                             <input type="file" name="file_upload" className="hidden" />
                         </label>
                     </div>
-                    <Button className="mt-4 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"> Post</Button>
                   
             </Card>
+                    <Button className="mt-4 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"> Post</Button>
         </div>
         </div>
     </div>
