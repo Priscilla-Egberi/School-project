1e0963e0d3fa60dfcd55ebf16bf5ca1769e50532
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-07 06:48:26 +0100

    working on AddGoods Component of the Account

--- src/components/account/AddGoods.js
+++ src/components/account/AddGoods.js
@@ -7,21 +7,33 @@ import {
 
 function AddGoods() {
   return (
-    <Card color="transparent" shadow={false}>
-                
-                <Typography color="gray" className="mt-1 font-normal">
-                  Enter your details to register.
-                </Typography>
-                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                  <div className="mb-4 flex flex-col gap-6">
-                    <Input size="lg" label="Product name" />
-                    <Input size="lg" label="Description" />
-                    <Input size="lg" label="Product price" />
-                    <Input size="lg" label="Conditions and features" />
-                    <Input size="lg" label="Preferred location" />                    
-                  </div>                  
-                </form>
-              </Card>
+    <>
+    <div className='grid grid-cols-3 gap-16'>
+        {/* ----------first col */}
+        <div className="col-span-2" >            
+                <Card color="transparent">                
+                            <Typography color="gray" className="mt-1 font-normal">
+                            Enter your details to register.
+                            </Typography>
+                            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                            <div className="mb-4 flex flex-col gap-6">
+                                <Input size="lg" label="Product name" />
+                                <Input size="lg" label="Description" />
+                                <Input size="lg" label="Product price" />
+                                <Input size="lg" label="Conditions and features" />
+                                <Input size="lg" label="Preferred location" />                    
+                            </div>                  
+                            </form>
+                </Card>
+        </div>
+        {/* ----------second col */}
+        <div className="col-span-1" >
+                        <Card>
+                            <Typography>Thank you Lord for who you are</Typography>
+                        </Card>
+        </div>
+      </div>
+    </>
   )
 }
 
