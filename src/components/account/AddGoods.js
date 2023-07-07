fa0db225c8c797f98206cbddc0c9c868af7f287d
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-07 07:02:36 +0100

    working on AddGoods Component of the Account

--- src/components/account/AddGoods.js
+++ src/components/account/AddGoods.js
@@ -8,21 +8,22 @@ import {
 function AddGoods() {
   return (
     <>
-    <div className='grid grid-cols-3 gap-16'>
+    <div className="px-4">
+        <div className='grid grid-cols-3 gap-16'>
         {/* ----------first col */}
-        <div className="col-span-2" >            
+        <div className="col-span-2 mx-auto" >            
                 <Card color="transparent">                
                             <Typography color="gray" className="mt-1 font-normal">
-                            Enter your details to register.
+                                Enter your details to register.
                             </Typography>
                             <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                            <div className="mb-4 flex flex-col gap-6">
-                                <Input size="lg" label="Product name" />
-                                <Input size="lg" label="Description" />
-                                <Input size="lg" label="Product price" />
-                                <Input size="lg" label="Conditions and features" />
-                                <Input size="lg" label="Preferred location" />                    
-                            </div>                  
+                                <div className="mb-4 flex flex-col gap-6">
+                                    <Input size="lg" label="Product name" />
+                                    <Input size="lg" label="Description" />
+                                    <Input size="lg" label="Product price" />
+                                    <Input size="lg" label="Conditions and features" />
+                                    <Input size="lg" label="Preferred location" />                    
+                                </div>                  
                             </form>
                 </Card>
         </div>
@@ -32,7 +33,8 @@ function AddGoods() {
                             <Typography>Thank you Lord for who you are</Typography>
                         </Card>
         </div>
-      </div>
+        </div>
+    </div>
     </>
   )
 }
