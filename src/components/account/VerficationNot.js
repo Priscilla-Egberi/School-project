9c213d98b42bed817abbe55a33e04a7749fcbc5a
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-07 08:07:03 +0100

    working on AddGoods Component of the Account

--- src/components/account/VerficationNot.js
+++ src/components/account/VerficationNot.js
@@ -10,11 +10,11 @@ function VerficationNot() {
       
         <div className="mx-auto mt-14 max-w-sm p-5 flex flex-col justify-center items-center ">
             <div className='rounded-full bg-my-lightorange p-6'>
-            <FaUserTimes />
+                <FaUserTimes size={32} />
             </div>
             <h1 className="my-3 text-xl font-bold tracking-tight text-gray-900 text-center">Your account is currently not verified</h1>
             <p className="text-center">You cannot sell on this platform without verifying your account as a student.<br/>Please providethe necessary requirements to indicate that you are a student of university of Uyo</p>
-            <Button className="mt-4 mx-3 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"> Verify your account</Button>
+                <Button className="mt-4 mx-3 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"> Verify your account</Button>
         </div>
        
       </div>
