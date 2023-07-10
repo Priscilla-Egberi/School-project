3dcd3ba12737d871b228563f7e15160531aaaf1d
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-10 18:46:06 +0100

    mobile view

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SideBar from "../components/account/SidebarAccount"
+import CollasibleSidebar from "../components/account/CollasibleSidebar"
 // import Navbar from "../components/account/NavbarAccount"
 import { Outlet } from 'react-router-dom'
 
@@ -10,6 +11,7 @@ function Account() {
         <section className="md:h-screen md:grid grid-cols-6">
               
                                   {/* ----------Sidebar */}
+                              <CollasibleSidebar />
                               <div className="hidden md:block col-span-1" >
                                   <SideBar />
                               </div>
