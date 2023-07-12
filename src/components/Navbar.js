e5be76eeb1614e8ccebfff4038e4d4b15ef4f5c4
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-12 20:16:01 +0100

    mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom"
 import {
   Navbar,
+  MobileNav,
   Typography,
   IconButton,
 } from "@material-tailwind/react";
@@ -10,6 +11,15 @@ import SearchComponent from "./SearchComponent";
 
  
 export default function Example() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+ 
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -76,10 +86,50 @@ export default function Example() {
             />
             </div>
             
-            
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
           </div>
         </div>
-        
+        <MobileNav open={openNav}>
+          {navList}
+          <SearchComponent variant="gradient" size="sm" fullWidth className="mb-2"/>
+          
+        </MobileNav>
       </Navbar>
       
     </>
