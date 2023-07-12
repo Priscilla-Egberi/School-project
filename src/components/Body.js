6f4205108224bef8fa1f58909641fca831f494d1
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-12 13:45:47 +0100

    mobile view

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,11 +8,11 @@ function Body() {
     <div classNameName="bg-white">
   <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
     <h2 className="text-2xl font-bold tracking-tight text-gray-900 flex justify-between items-center">
-      <p className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</p>
+      <p className="text-2xl font-bold tracking-tight text-gray-900">Electronics</p>
       <p>See more <IoArrowForwardSharp className="ml-3" /></p>
       </h2>
 
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+    <div className="mt-6 grid grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
     <Cards detail="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" />
       <Cards detail="https://images.unsplash.com/photo-1585298723682-7115561c51b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGVhZHBob25lc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" />
       <Cards detail="https://plus.unsplash.com/premium_photo-1671611822374-4719df5c89bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2lyZWxlc3MlMjBtb3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" />
