33c91499870b2608387285f2f2ebf6af51386a21
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-13 11:41:38 +0100

    mobile view

--- src/components/Goods/DropDown.js
+++ src/components/Goods/DropDown.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { IoChevronDownSharp } from "react-icons/io5"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -12,7 +12,7 @@ export default function Example() {
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Options
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+          <IoChevronDownSharp className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
 
