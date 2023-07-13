1d307e1c16adf6b296344c70de43cf5399d06a3b
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-13 06:20:32 +0100

    mobile view

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,6 +1,7 @@
 import Home from './Home'
 import Login from "./Login"
 import Account from "./Account"
+import Goods from "./Goods"
 import {Routes, Route} from "react-router-dom"
 import RoutableHomeSide from '../components/RoutableHomeSide'
 import Electronics from "../components/Electronics"
@@ -11,7 +12,7 @@ import Body from "../components/account/BodyAccount"
 import AddItem from "../components/account/AddNewItem"
 import VerficationNot from '../components/account/VerficationNot'
 import Verified from '../components/account/Verified'
-import Goods from '../components/account/Goods'
+import AccountGoods from '../components/account/Goods'
 import Services from '../components/account/Services'
 import AddGoods from '../components/account/AddGoods'
 import AddServices from '../components/account/AddServices'
@@ -33,6 +34,10 @@ export default function pages() {
           <Route path="/product" element={<Product />}/>
         </Route>
         <Route path="/login" element={<Login />}/>
+        {/* ------------------- GOODS */}
+        <Route path="/goods" element={<Goods />}>
+
+        </Route>
         {/* -------------------ACCOUNT */}
         <Route path="/account" element={<Account />}>
                 <Route path="/account" element={<Body />}/>
@@ -43,7 +48,7 @@ export default function pages() {
                 </Route>
                 <Route path="/account/verification" element={<VerficationNot />}/>
                 <Route path="/account/verified" element={<Verified />}/>
-                <Route path="/account/goods" element={<Goods />}/>
+                <Route path="/account/goods" element={<AccountGoods />}/>
                 <Route path="/account/services" element={<Services />}/>
         </Route>
       </Routes>
