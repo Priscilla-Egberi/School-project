4d8d416410ed3275455c737a17d4b8ce5b2e2d44
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-16 07:01:15 +0100

    mobile view

--- src/components/CheckHomContext.js
+++ src/components/CheckHomContext.js
@@ -3,12 +3,12 @@ import React, {createContext, useState} from 'react'
 
 const CheckHomContext = createContext()
 
-export function CheckHomContext({children}) {
+export function CheckHomContext() {
     const [checks, setChecks] = useState('true');
 
   return (
     <CheckHomContext.Provider value={[checks, setChecks]}>
-      {children}
+      
     </CheckHomContext.Provider>
   )
 }
