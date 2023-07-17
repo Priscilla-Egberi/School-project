74d4f7080c2719050038292f7db390ec3925a788
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-17 16:13:29 +0100

    mobile view

--- src/components/CollapsibleSidebar.js
+++ src/components/CollapsibleSidebar.js
@@ -30,7 +30,7 @@ function SideBar() {
     <section onClick={()=>{setChecks(true)}}  id="openSidebarHere" className={`side-bar md:hidden ${checks? "":'openSidebar'} font-semibold h-full bg-white flex flex-col justify-between border-r-gray border-2`}>
         <div>
               <div id="title" className="flex flex-row items-center justify-between pl-4 py-3 text-xl font-bold tracking-tight"> 
-              <p className='flex'>CampusBuy <img src={logo} alt="logo" className='inline w-6' /></p>
+              <p className='flex'>CampusBuy <img src={logo} alt="logo" className='ml-3 inline w-6' /></p>
               <div className="inline">
                 <IoClose size={32} className="ml-3  lg:hidden" id="openSidebar"/>
             </div>
