378361f6118db352b66c9f54d7a9f5fcc617374f
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-18 10:54:35 +0100

    mobile view

--- src/login/Login.js
+++ src/login/Login.js
@@ -22,7 +22,7 @@ function Login() {
 	<div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
 		<form className="bg-white">
 			<h1 className="text-gray-800 font-bold text-2xl mb-1">Hello!</h1>
-			<p className="text-sm font-normal text-gray-600 mb-7">You don't have an account? <Link to="/signup" className="text-my-orange">Sign up</Link></p>
+			<p className="text-sm font-normal text-gray-600 mb-7">You don't have an account? <Link to="/signup" className="text-my-orange hover:text-black cursor-pointer">Sign up</Link></p>
 			{/* <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
 				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
 					fill="currentColor">
