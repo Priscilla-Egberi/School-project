86d8c9967e22453f433eea7092427f0797bf0851
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-18 14:29:37 +0100

    mobile view

--- src/login/Login.js
+++ src/login/Login.js
@@ -30,7 +30,7 @@ function Login() {
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
 								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
 						</svg>
-						<input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+						<input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" required />
       </div>
 						<div className="flex items-center border-2 py-2 px-3 rounded-2xl">
 							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
@@ -39,7 +39,7 @@ function Login() {
 									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
 									clip-rule="evenodd" />
 							</svg>
-							<input classNameName="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
+							<input classNameName="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" required />
       </div>
 							<button type="submit" className="block w-full bg-my-orange mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
 							<span className="text-sm ml-2 hover:text-my-orange cursor-pointer">Forgot Password ?</span>
