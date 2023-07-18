796b603731f7ef8d0f58413256a4a9a807263f19
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-18 11:45:48 +0100

    mobile view

--- src/login/SignUp.js
+++ src/login/SignUp.js
@@ -40,7 +40,7 @@ function SignUpForm() {
 		<div>
 			<h1 className="text-white font-bold text-4xl font-sans">Get Empowered</h1>
 			<p className="text-white mt-1">Show what you have. Get what you need</p>
-			<Link to="/login/signup" className="block w-28 bg-white text-orange-800 mt-4 py-2 rounded-2xl font-bold mb-2">Sign up</Link>
+			<Link to="/login" className="block w-28 bg-white text-orange-800 mt-4 py-2 rounded-2xl font-bold mb-2">Login</Link>
 		</div>
 		<div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
 		<div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
@@ -50,7 +50,7 @@ function SignUpForm() {
 	<div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
 		<form onSubmit={handleSubmit} className="bg-white">
 			<h1 className="text-gray-800 font-bold text-2xl mb-1">Hello!</h1>
-			<p className="text-sm font-normal text-gray-600 mb-7">You don't have an account? <Link to="/login/signup" className="text-my-orange hover:text-black cursor-pointer">Sign up</Link></p>
+			<p className="text-sm font-normal text-gray-600 mb-7">Already have an account? <Link to="/login" className="text-my-orange hover:text-black cursor-pointer ">Login</Link></p>
 			<div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
 				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
 					fill="currentColor">
@@ -83,14 +83,6 @@ function SignUpForm() {
                             onChange={handleChange}
                             />
       </div>			
-				{/* <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
-						viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-							d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
-					</svg>
-					<input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Reg Number" />
-      </div> */}
 					<div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
 							viewBox="0 0 24 24" stroke="currentColor">
@@ -120,7 +112,7 @@ function SignUpForm() {
                                 type="password"
                                 id="password"
                                 name="password"
-                                placeholder="email"
+                                placeholder="password"
                                 value={formData.password}
                                 onChange={handleChange}
                                 />
