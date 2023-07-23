bec90c0f84e29e0f2b510090cd1666d4f7b5af77
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-23 16:18:19 +0100

    editing the service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>
-       <div className="flex items-start justify-between px-16 py-4">
+    <div className='py-4'>
+       <div className="flex items-start justify-between px-16">
        <div className='flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
                   alt="" 
@@ -16,7 +16,7 @@ function Services() {
           </div>
           <p className="text-blue-700 pt-6">Not Available</p>
        </div>
-          <p className='px-16'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo modi quam labore aliquid officia illo ipsam commodi dolore excepturi unde numquam, eveniet qui itaque delectus dolorum quasi a et repellat eum beatae aut incidunt, tenetur veniam exercitationem? Magnam odio sunt ut cum, ratione laboriosam tenetur voluptatum sapiente reprehenderit laudantium corporis fugit ipsam id nulla, in iusto voluptatem esse quasi! Magni veniam possimus, aut architecto fuga sint sunt a accusantium, maiores explicabo repellendus nobis voluptas ut doloribus culpa consequuntur accusamus commodi! Sed velit earum voluptas veritatis dolor alias, laudantium amet eum nihil deserunt. Commodi quae dignissimos libero odit possimus ad porro.</p>
+       <p className='px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
   )
 }
