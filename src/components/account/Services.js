099ec7766900cc12b7698646c2967c1f652bc89b
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-23 17:13:06 +0100

    editing the service page

--- src/components/account/Services.js
+++ src/components/account/Services.js
@@ -13,8 +13,7 @@ function Services() {
 
   return (
     <div>
-    <div className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900 flex items-center justify-between'>Goods
-        Services
+    <div className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900 flex items-center justify-between'>Services
         <div className="inline">
                   <input type="checkbox" id="toggle" className="hidden" onChange={handleChange} value={check} />
                   <label htmlFor="toggle"><FaBars size={32} className="ml-3  lg:hidden" id="openSidebar"/></label>
