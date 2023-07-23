c27f66cbbf75300038080b9143332ae8c03e38d5
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-23 16:01:13 +0100

    editing the service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,15 +3,19 @@ import React from 'react'
 function Services() {
   return (
     <div>
+       <div className="flex items-start justify-between">
        <div className='my-4 px-16 flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
                   alt="" 
                   className="w-24 h-24 rounded-full border-4 border-gray-700 my-0 mr-4" />
                   <div>
                     <h1 className="text-2xl font-bold tracking-tight text-gray-900">Julian Benedict</h1>
-                    <p>17/EG/CO/1035</p>
+                    <p>Hairstylist</p>
+                    <p>Nwaniba</p>
                   </div>
           </div>
+          <p className="text-blue-700">Not Available</p>
+       </div>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo modi quam labore aliquid officia illo ipsam commodi dolore excepturi unde numquam, eveniet qui itaque delectus dolorum quasi a et repellat eum beatae aut incidunt, tenetur veniam exercitationem? Magnam odio sunt ut cum, ratione laboriosam tenetur voluptatum sapiente reprehenderit laudantium corporis fugit ipsam id nulla, in iusto voluptatem esse quasi! Magni veniam possimus, aut architecto fuga sint sunt a accusantium, maiores explicabo repellendus nobis voluptas ut doloribus culpa consequuntur accusamus commodi! Sed velit earum voluptas veritatis dolor alias, laudantium amet eum nihil deserunt. Commodi quae dignissimos libero odit possimus ad porro.</p>
     </div>
   )
