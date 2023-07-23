a2f751b4300f285cdde9d9c7eb1a9b434fa3e546
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-23 18:03:30 +0100

    editing the service page

--- src/pages/Images.js
+++ src/pages/Images.js
@@ -26,10 +26,37 @@ const ImageUploader = () => {
       <label
         className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none"
       >
-        {/* ... */}
-        {/* The rest of the label and input elements remain the same */}
+        <span className="flex items-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 text-gray-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+            />
+          </svg>
+          <span className="font-medium text-gray-600">
+            {imagePreviews.length === 0
+              ? "Drop Item's Image to Attach, or"
+              : 'Uploaded Images:'}
+            <span className="text-blue-600 underline"> browse</span>
+          </span>
+        </span>
+        <input
+          type="file"
+          name="file_upload"
+          className="hidden"
+          accept="image/*"
+          multiple
+          onChange={handleImageChange}
+        />
       </label>
-
       {imagePreviews.length > 0 && (
         <div className="grid grid-cols-3 gap-4">
           {imagePreviews.map((imageURL, index) => (
@@ -37,14 +64,14 @@ const ImageUploader = () => {
               key={index}
               src={imageURL}
               alt={`Image ${index}`}
-              style={{ width: '100%', height: '150px' }}
+              style={{ width: '150px', height: '150px', margin: '10px' }}
             />
           ))}
         </div>
       )}
 
       <button
-        className="mt-4 rounded-sm flex flex-row items-center justify-center py-3 text-white bg-my-orange"
+        className="w-full mt-4 rounded-sm text-center py-3 text-white bg-my-orange"
         onClick={handlePost}
       >
         Post
