61f20258fda82ba26d71e7fbc73acbfccf92effa
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-23 16:27:59 +0100

    editing the service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='py-4'>
+    <>
+    <div className='py-4 border-b-2 border-gray'>
        <div className="flex items-start justify-between px-16">
        <div className='flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
@@ -18,6 +19,24 @@ function Services() {
        </div>
        <p className='px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
+    
+    <div className='py-4 border-b-2 border-gray'>
+       <div className="flex items-start justify-between px-16">
+       <div className='flex flex-row items-center'>
+                  <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
+                  alt="" 
+                  className="w-24 h-24 rounded-full border-4 border-gray-700 my-0 mr-4" />
+                  <div>
+                    <h1 className="text-2xl font-bold tracking-tight text-gray-900">Julian Benedict</h1>
+                    <p>Hairstylist</p>
+                    <p>Nwaniba</p>
+                  </div>
+          </div>
+          <p className="text-blue-700 pt-6">Not Available</p>
+       </div>
+       <p className='px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
+    </div>
+    </>
   )
 }
 
