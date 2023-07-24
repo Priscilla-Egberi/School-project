e52743f860c0e29be1d376187d92d7433dfd6d6c
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-24 21:44:52 +0100

    editing the service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -14,7 +14,7 @@ function Services() {
         
     <div className="mx-10 md:mx-30">
     <div className='py-4 border-b-2 border-gray'>
-       <div className="flex items-start justify-between px-16">
+       <div className="flex items-start justify-between md:px-16">
        <div className='flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
                   alt="" 
@@ -27,11 +27,11 @@ function Services() {
           </div>
           <p className="text-blue-700 pt-6">Not Available</p>
        </div>
-       <p className='px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
+       <p className='md:px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
     
     <div className='py-4 border-b-2 border-gray'>
-       <div className="flex items-start justify-between px-16">
+       <div className="flex items-start justify-between md:px-16">
        <div className='flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
                   alt="" 
@@ -44,7 +44,7 @@ function Services() {
           </div>
           <p className="text-blue-700 pt-6">Not Available</p>
        </div>
-       <p className='px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
+       <p className='md:px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
     </div>
     </>
