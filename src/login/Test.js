3bd799629c7ccfffcad29186a3aae4e39a9ceb7a
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-24 17:03:36 +0100

    editing the service page

--- src/login/Test.js
+++ src/login/Test.js
@@ -1,19 +1,21 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react';
 import axiosInstance from './Axios';
 import { UserContext } from './UserContext';
 
-const { user, setUser } = useContext(UserContext);
-axiosInstance.get('https://campus-buy.vercel.app/user').then((res) => {
-  // console.log(res.data);
-  setUser(res.data)
-});
-
-console.log(user)
-
 function Test() {
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    axiosInstance.get('https://campus-buy.vercel.app/user').then((res) => {
+      setUser(res.data);
+    });
+  }, []);
+
+  console.log(user);
+
   return (
     <div>Test</div>
-  )
+  );
 }
 
-export default Test
+export default Test;
