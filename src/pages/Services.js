6209bb421a173c62f7b38854ba9c57f75cb3d83c
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-24 22:25:39 +0100

    editing the service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -29,23 +29,40 @@ function Services() {
        </div>
        <p className='md:px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
-    
-    <div className='py-4 border-b-2 border-gray'>
-       <div className="flex items-start justify-between md:px-16">
+    <div className='py-4 border-b-2 border-gray-300'>
+       <div className="md:flex items-start justify-between md:px-16">
        <div className='flex flex-row items-center'>
                   <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
                   alt="" 
-                  className="max-w-24 max-h-6 rounded-full border-4 border-gray-700 my-0 mr-4" />
-                  <div>
-                    <h1 className="text-2xl font-bold tracking-tight text-gray-900">Julian Benedict</h1>
+                  className="max-w-96 max-h-36 min-w-54 min-h-54 rounded-full border-4 border-gray-700 my-0 mr-4" />
+                  <div className='leading-none md:leading-normal'>
+                    <h1 className="text-xl md:text-2xl font-bold tracking-tight text-gray-900">Julian Benedict</h1>
                     <p className='font-semibold'>Hairstylist</p>
                     <p className='font-semibold text-gray-600'>See resume</p>
                   </div>
           </div>
-          <p className="text-blue-700 pt-6">Not Available</p>
+          <p className="text-blue-700 pt-3 md:pt-6">Not Available</p>
        </div>
        <p className='md:px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
     </div>
+    <div className='py-4 border-b-2 border-gray-300'>
+       <div className="md:flex items-start justify-between md:px-16">
+       <div className='flex flex-row items-center'>
+                  <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" 
+                  alt="" 
+                  className="max-w-96 max-h-36 min-w-54 min-h-54 rounded-full border-4 border-gray-700 my-0 mr-4" />
+                  <div className='leading-none md:leading-normal'>
+                    <h1 className="text-xl md:text-2xl font-bold tracking-tight text-gray-900">Julian Benedict</h1>
+                    <p className='font-semibold'>Hairstylist</p>
+                    <p className='font-semibold text-gray-600'>See resume</p>
+                  </div>
+          </div>
+          <p className="text-blue-700 pt-3 md:pt-6">Not Available</p>
+       </div>
+       <p className='md:px-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque expedita aliquam voluptatum eius, dolores eveniet id pariatur explicabo cupiditate?</p>
+    </div>
+    
+   
     </div>
     </>
   )
