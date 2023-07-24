4e14c226cec7a0cc2c9bf6017d7106cc2a3bfa58
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-24 23:10:22 +0100

    editing the service page

--- src/components/account/AddServices.js
+++ src/components/account/AddServices.js
@@ -13,11 +13,11 @@ function AddServices() {
         <div className='md:grid grid-cols-6 gap-16'>
         {/* ----------first col */}
         <div className="col-span-3" >            
-                <Card color="transparent" className=' px-4 py-3 max-w-max'>                
+                  <Card color="transparent" className=' px-4 py-3 w-full md:max-w-max'>                
                             <Typography color="gray" className="mt-1 font-normal">
                                 Enter your details to register.
                             </Typography>
-                            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                            <form className="mt-8 mb-2 max-w-screen-lg md:w-96 ">
                                 <div className="mb-4 flex flex-col gap-6">
                                     <Input size="lg" label="Service name" />
                                     <Input size="lg" label="Description" />
