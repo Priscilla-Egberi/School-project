ba19cbb5716b564961d8c3a6507530bb76dacb39
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-25 04:29:07 +0100

    Worked on account Page

--- src/components/account/BodyAccount.js
+++ src/components/account/BodyAccount.js
@@ -50,7 +50,7 @@ export function BodyAccount () {
       <img
         src={uploadedImage}
         alt=""
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover object-center"
       />
 
       {/* The file input element is hidden and triggered when the div is clicked */}
