c5f9b979d7aa56fb302ba43b540ef536965de811
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-25 22:41:06 +0100

    Working on the account profile page

--- src/components/account/BodyAccount.js
+++ src/components/account/BodyAccount.js
@@ -72,13 +72,13 @@ export function BodyAccount () {
           {/* table 1 */}
           <div className="border border-gray-300 rounded">
                             <h1 className="px-5 py-2 border-b border-gray-300 text-xl font-bold tracking-tight text-gray-900 ">Information</h1>
-                            <div className="px-5 my-3">
+                            <label className="px-5 my-3">
                                 <p className="text-sm text-gray-500 dark:text-gray-400">Display Name</p>
                                 <div className="flex flex-row justify-between">
-                                    <p className="text-xl font-bold tracking-tight text-gray-900">Juilan Benedict</p>
-                                    <p className="bg-gray-300 hover:bg-gray-600 font-semibold text-sm hover:text-gray-200 py-2 px-3 rounded">Edit</p>
+                                    <input className="text-xl font-bold tracking-tight text-gray-900" placeholder='Juilan Benedict' />
+                                    <button className="bg-gray-300 hover:bg-gray-600 font-semibold text-sm hover:text-gray-200 py-2 px-3 rounded">Edit</button>
                                 </div>
-                            </div>
+                            </label>
                             <div className="px-5 my-3">
                                 <p className="text-sm text-gray-500 dark:text-gray-400">Department</p>
                                 <div className="flex flex-row justify-between">
