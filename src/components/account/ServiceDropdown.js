91e523b441496467c1a4a0527c94dd6f1aca3bf5
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-25 11:15:58 +0100

    Worked on account Page

--- src/components/account/ServiceDropdown.js
+++ src/components/account/ServiceDropdown.js
@@ -12,7 +12,6 @@ export default function Example() {
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           ...
-          <IoChevronDownSharp className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
         </Menu.Button>
       </div>
 
