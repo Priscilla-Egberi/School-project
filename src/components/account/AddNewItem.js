3580d5a4476be41b7dbca0bb497f4f47d5015ed9
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-25 19:26:19 +0100

    Worked on account Page

--- src/components/account/AddNewItem.js
+++ src/components/account/AddNewItem.js
@@ -5,7 +5,8 @@ function AddNewItem() {
   return (
     <div>
       <p className='py-3 px-16 border-b-2 border-gray text-xl font-bold tracking-tight text-gray-900'>Add New Item</p>
-      <h1 className="mt-14 mb-7 text-xl font-bold tracking-tight text-gray-900 text-center">What do you want to add?</h1>
+      {/* <h1 className="mt-14 mb-7 text-xl font-bold tracking-tight text-gray-900 text-center">What do you want to add?</h1> */}
+      <div className="h-4"></div>
         <Outlet />
       </div>
   )
