23cf25d9d0024eec0bc159a2c5039c2210b0c409
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-26 22:45:42 +0100

    Working on the account profile page

--- src/data/service.js
+++ src/data/service.js
@@ -1,17 +1,323 @@
-export const Img =[
+
+export const serviceData = [
   {
       id: 1,
-      name: "Julian Benedict",
-      amount: "#4352",
-      condition: "fairly used",
-      category: "Home Utensils",
-      addressForPickUp: "Address 1",
+      name: "Juliet James",
+      department: "Petroleum Engineering",
+      level: "200",
       imgUrl: [
           "https://via.placeholder.com/500?text=Image1&bg=222",
           "https://via.placeholder.com/500?text=Image1&bg=444",
           "https://via.placeholder.com/500?text=Image1&bg=666"
       ],
-      userId: 14
-  },
-
+      category:"Hair Dressing",
+      service:"Hair Dresser",
+      description:"I am passionate about hair, I make all styles of hair, including knotless braid",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/8.jpg",
+userId:1
+},
+  {
+      id: 2,
+      name: "Olivia Henry",
+      department: "Petroleum Engineering",
+      level: "200",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tutoring",
+service:"Tutor",
+      description:"I love teaching students to understand. I am good at Engine Maths",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/6.jpg",
+userId:2
+},
+  {
+      id: 3,
+      name: "Sandra Benjamin",
+      department: "Petroleum Engineering",
+      level: "500",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tutoring",
+service:"Tutor",
+      description:"I love teaching students to understand. I am good at Engine Maths",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/7.jpg",
+userId:3
+},
+  {
+      id: 4,
+      name: "Kehla Leo",
+      department: "Electrical Engineering",
+      level: "400",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Laptop and Phone Repairs",
+      service:"Technician",
+      description:"Hi, I fix laptops and phones. I have worked on different problems on both Phones and Laptops.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/1.jpg",
+userId:4
+},
+  {
+      id: 5,
+      name: "Olivia Leo",
+      department: "Mechanical Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tutoring",
+service:"Tutor",
+      description:"I love teaching students to understand. I am good at Engine Maths",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/8.jpg",
+userId:5
+},
+  {
+      id: 6,
+      name: "Chiwendu Okafor",
+      department: "Petroleum Engineering",
+      level: "200",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Laptop and Phone Repairs",
+      service:"Phone Fixer",
+      description:"Hi, I fix laptops and phones. I have worked on different problems on both Phones and Laptops.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/1.jpg",
+userId:6
+},
+  {
+      id: 7,
+      name: "James Johnson",
+      department: "Electrical Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"House Painting",
+      service:"Painter",
+      description:"hi, I do house painting, I literally can draw a human on the wall, my skills are top notch, test and see for yourself",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/9.jpg",
+userId:7
+},
+  {
+      id: 8,
+      name: "Daniel Augustine",
+      department: "Food Engineering",
+      level: "400",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Catering",
+      service:"Caterer",
+      description:"I bake cake, cupcake, meat pie. I can make realistic cakes too. ",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/1.jpg",
+userId:8
+},
+  {
+      id: 9,
+      name: "Ethan Michael",
+      department: "Chemical Engineering",
+      level: "300",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tutoring",
+service:"Tutor",
+      description:"I love teaching students to understand. I am good at Engine Maths",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/2.jpg",
+userId:9
+},
+  {
+      id: 10,
+      name: "Elijah Monye",
+      department: "Computer Engineering",
+      level: "300",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Hair Dressing",
+      service:"Hair Dresser",
+      description:"I am passionate about hair, I make all styles of hair, including knotless braid",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/3.jpg",
+userId:10
+},
+  {
+      id: 11,
+      name: "Liatle Drag",
+      department: "Mechanical Engineering",
+      level: "400",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Catering",
+      service:"Chef",
+      description:"Hi, I'm chef, I cook African delicies such as Afang soup, Edikan Soup, Okra soup and so on. I surely deliver",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/1.jpg",
+userId:11
+},
+  {
+      id: 12,
+      name: "Gabriella White",
+      department: "Computer Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Hair Dressing",
+      service:"Hair Dressing",
+      description:"I am passionate about hair, I make all styles of hair, including knotless braid",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/2.jpg",
+userId:12
+},
+  {
+      id: 13,
+      name: "Jackson Sebastian",
+      department: "Petroleum Engineering",
+      level: "300",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Catering",
+      service:"Caterer",
+      description:"I bake cake, cupcake, meat pie. I can make realistic cakes too. ",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/4.jpg",
+userId:13
+},
+  {
+      id: 14,
+      name: "Andrew Crutle",
+      department: "Petroleum Engineering",
+      level: "400",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tech related Jobs",
+      service:"Front-End web developer",
+      description:"Hello there, I am a web developer, I have 3years experience building with React, tailwind, HTML and CSS",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/5.jpg",
+userId:14
+},
+  {
+      id: 15,
+      name: "Precious Nwachi",
+      department: "Civil Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tech related Jobs",
+      service:"Full Stack developer",
+      description:"Hello there, I am a web developer, I have 3years experience building with Nodejs, React, tailwind, HTML and CSS",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/3.jpg",
+userId:15
+},
+  {
+      id: 16,
+      name: "Jesica Obama",
+      department: "Electrical Engineering",
+      level: "200",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tech related Jobs",
+      service:"Graphics Designer",
+      description:"I am a graphics desiner, I design besically anything, such as birthday cards, invitation cards, fliers etc.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/4.jpg",
+userId:16
+},
+  {
+      id: 17,
+      name: "Thomas Trump",
+      department: "Petroleum Engineering",
+      level: "200",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tailoring",
+      service:"Tailor",
+      description:"Tailoring is my passion, I sew difference kinds of male clothes, Senator suit, hand made suit etc and I sew them very well.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/6.jpg",
+userId:17
+},
+  {
+      id: 18,
+      name: "Alexander Koit",
+      department: "Electrical Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Others",
+      service:"House Agent",
+      description:"I help you search, for comfortable, secured and affordable house. My price is negotiable.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/female/5.jpg",
+userId:18
+},
+  {
+      id: 19,
+      name: "Samuel Nathaniel",
+      department: "Petroleum Engineering",
+      level: "100",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"House Painting",
+      service:"House Painter",
+      description:"hi, I do house painting, I literally can draw a human on the wall, my skills are top notch, test and see for yourself",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/7.jpg",
+userId:19
+},
+  {
+      id: 20,
+      name: "George Bush",
+      department: "Mechanical Engineering",
+      level: "200",
+      imgUrl: [
+          "https://via.placeholder.com/500?text=Image1&bg=222",
+          "https://via.placeholder.com/500?text=Image1&bg=444",
+          "https://via.placeholder.com/500?text=Image1&bg=666"
+      ],
+      category:"Tailoring",
+      service:"Tailor",
+      description:"Tailoring is my passion, I sew difference kinds of male clothes, Senator suit, hand made suit etc and I sew them very well.",
+      imgUrl: "https://xsgames.co/randomusers/assets/avatars/male/8.jpg",
+      userId:20
+  }
 ]
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -7,7 +7,7 @@ function Services() {
     <>
         <h2 className="mt-2 md:mt-20 text-2xl semibold md:hidden ml-3 my-3">Goods</h2>
         <div className="md:mx-20 mx-5 md:my-12 g-y-5">
-        {/* <Search variant="gradient" size="sm" fullWidth className="mb-5" data={Img}/> */}
+        {/* <Search variant="gradient" size="sm" fullWidth className="mb-5" data={}/> */}
         </div>
         
     <div className="mx-5 md:mx-10">
