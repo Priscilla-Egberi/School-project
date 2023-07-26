6d0f5592845848b8ffc886cae22ab0034fbce458
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-27 00:32:29 +0100

    Working on the account profile page

--- src/components/sidebarPages/Tutoring.js
+++ src/components/sidebarPages/Tutoring.js
@@ -12,7 +12,7 @@ console.log(filteredTutoring);
   <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
     <h2 className="text-2xl font-bold tracking-tight text-gray-900">Tutoring</h2>
 
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+    <div className="mt-6  ">
         {filteredTutoring.map((item) => (
           <div key={item.id}>
                                   
