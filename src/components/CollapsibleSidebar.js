42ad3e48f78bd49fcf5249871be5f5d87829c30d
Priscilla-Egberi <priscillaegberi@gmail.com>
2023-07-26 19:24:50 +0100

    Working on the account profile page

--- src/components/CollapsibleSidebar.js
+++ src/components/CollapsibleSidebar.js
@@ -38,25 +38,25 @@ function SideBar() {
             <p className="flex items-center"><TbDotsCircleHorizontal className="mr-2"/>Others</p> */}
         </div>
 
-              <div className="px-4  flex flex-col gap-y-3">
+        <div className="px-4  flex flex-col gap-y-3">
             <p className="text-gray-500 mt-7 mb-4">Goods______________________</p>
             <Link to="/electronics" className="flex items-center"><MdComputer className="mr-2"/> Electronics</Link>
             <Link to="/clothings" className="flex items-center"><IoShirtOutline  className="mr-2"/> Clothings and Jewelries</Link>
-            <p className="flex items-center"><GiPencilRuler className="mr-2"/> School materials</p>
-            <p className="flex items-center"><GiComputerFan className="mr-2"/> Home Utensils</p>
-            <p className="flex items-center"><CgGirl className="mr-2"/> Health and Beauty</p>
-            <p className="flex items-center"><TbDotsCircleHorizontal className="mr-2"/>Others</p>
+            <Link to="/schoolMaterial" className="flex items-center"><GiPencilRuler className="mr-2"/> School materials</Link>
+            <Link to="/homeUtensils" className="flex items-center"><GiComputerFan className="mr-2"/> Home Utensils</Link>
+            <Link to="/healthBeauty" className="flex items-center"><CgGirl className="mr-2"/> Health and Beauty</Link>
+            <Link to="/othersGoods" className="flex items-center"><TbDotsCircleHorizontal className="mr-2"/>Others</Link>
         </div>
         <div className="px-4 mt-4 mb-8 flex flex-col gap-y-3">
             <p className="text-gray-500 my-4">Services_____________________</p>
             <Link to="/tutorial" className="flex items-center"><BiBookBookmark className="mr-2"/> Tutoring</Link>
-            <p className="flex items-center"><TfiPaintRoller className="mr-2"/> House Painting</p>
-            <p className="flex items-center"><CgGirl className="mr-2"/> Hair Dressing</p>
-            <p className="flex items-center"><MdComputer className="mr-2"/> Laptop and Phone Repairs</p>
-            <p className="flex items-center"><CiForkAndKnife className="mr-2"/> Catering</p>
-            <p className="flex items-center"><MdComputer className="mr-2"/> Tech related Jobs</p>
-            <p className="flex items-center"><TiScissorsOutline className="mr-2"/> Tailoring</p>
-            <p className="flex items-center"><TbDotsCircleHorizontal className="mr-2"/> Others</p>
+            <Link to="/housePainting" className="flex items-center"><TfiPaintRoller className="mr-2"/> House Painting</Link>
+            <Link to="/hairDressing" className="flex items-center"><CgGirl className="mr-2"/> Hair Dressing</Link>
+            <Link to="/laptopRepairs" className="flex items-center"><MdComputer className="mr-2"/> Laptop and Phone Repairs</Link>
+            <Link to="/catering" className="flex items-center"><CiForkAndKnife className="mr-2"/> Catering</Link>
+            <Link to="/techJob" className="flex items-center"><MdComputer className="mr-2"/> Tech related Jobs</Link>
+            <Link to="/tailoring" className="flex items-center"><TiScissorsOutline className="mr-2"/> Tailoring</Link>
+            <Link to="/othersService" className="flex items-center"><TbDotsCircleHorizontal className="mr-2"/> Others</Link>
         </div>
         </div>
           
